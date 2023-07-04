e98b8532b23d757b60364fa8f76ff755fc733af4
VMDeepthi <deepthim@brightcomgroup.com>
2023-07-04 17:50:38 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+//import React, { useEffect, useState } from 'react'
+import {Component} from 'react'
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
 
-function App() {
+
+import Login from './components/Login'
+import Home from './components/Home'
+import Products from './components/Products'
+import ProductItemDetails from './components/ProductItemDetails'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute';
+import CheckOut from './components/CheckOut'
+import Payment from './components/Payment'
+import Order from './components/Orderlist'
+import ContactUs from './components/ContactUs'
+import { ProductCountProvider } from './context/ProductCountContext'
+
+class App extends Component {
+     
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <ProductCountProvider>
+      <Routes>
+          
+          <Route path='/login' Component={Login}/>
+          <Route path='/' element={<ProtectedRoute component={<Home />} />} />
+          <Route path='/products' element={<ProtectedRoute component={<Products />} />} />
+          <Route path='/products/:id' element={<ProtectedRoute component={<ProductItemDetails />} />}/>
+          <Route path='/cart' element={<ProtectedRoute component={<Cart />} />} />
+          <Route path='/checkout' element={<ProtectedRoute component={<CheckOut />} />} />
+          <Route path='/payment' element={<ProtectedRoute component={<Payment />} />} />
+          <Route path='/myorders' element={<ProtectedRoute component={<Order />} />} />
+          <Route path='/contactus' element={<ProtectedRoute component={<ContactUs />} />} />
+          <Route  path = "/not-found" Component={NotFound}/>
+          <Route  path='*' element={<Navigate replace to = '/not-found' />} />
+      </Routes>
+      </ProductCountProvider>
+      
+    
+    </BrowserRouter>
+  )
 }
-
-export default App;
+  }
+export default App
